27599ed22c2acc2dc56d978e2432a5b9c8e8bae8
Giaffreda <luca.giaffreda3@gmail.com>
2019-01-25 15:37:34 +0100

    Modifica5

--- DressShop/src/model/UserBeanDM.java
+++ DressShop/src/model/UserBeanDM.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.LinkedList;
-
+//ok
 public class UserBeanDM implements UserModel<UserBean> {
 	
 	private static final String  TABLE_NAME ="utenti";
