c31892a58d59c2d9a626cd6f221218375512b765
Rober-97 <43641606+Rober-97@users.noreply.github.com>
2019-01-26 18:55:01 +0100

    Update DressShop.sql

--- Bozze/DB/DressShop.sql
+++ Bozze/DB/DressShop.sql
@@ -8,23 +8,13 @@ nome			varchar(30),
 cognome			varchar(30),
 email			varchar(30)	not null,
 password		varchar(16)	not null,
-tipo			varchar(3)
+tipo			varchar(3),
 /*tipo:	cli=cliente
 	mag=magazziniere
 	mkt=gestore marketing
 	pro=proprietario
 */
-);
-
-CREATE TABLE CLIENTE(
-id_cliente		int		primary key,
-data_nascita		date,
-cellulare		varchar(13),
-
-	foreign key (id_cliente)
-	references UTENTE(id_utente)
-		on delete cascade
-		on update cascade
+data_nascita		date
 );
 
 CREATE TABLE CARTA_CREDITO(
@@ -33,24 +23,27 @@ data_scadenza		date		not null,
 cvv			char(3)		not null,
 nome_proprietario	varchar(30)	not null,
 cognome_proprietario	varchar(30)	not null,
-cliente			int,
+utente			int,
 
-	foreign key (cliente)
-	references CLIENTE(id_cliente)
+	foreign key (utente)
+	references UTENTE(id_utente)
 		on delete no action	
 		on update cascade
 );
 
 CREATE TABLE INDIRIZZO(
 id_indirizzo		int		primary key auto_increment,
+nome			varchar(30),
+cognome			varchar(30),
 cap			char(5),
 provincia		char(2),
 citta			varchar(30),
 via			varchar(50),
-cliente			int,
+cellulare		varchar(13),
+utente			int,
 
-	foreign key (cliente)
-	references CLIENTE(id_cliente)
+	foreign key (utente)
+	references UTENTE(id_utente)
 		on delete no action
 		on update cascade
 );
@@ -61,7 +54,7 @@ data			date,
 pagato			boolean,
 carta_credito		varchar(16),
 indirizzo		int,
-cliente			int,
+utente			int,
 tipo_spedizione		varchar(10),
 costo_spedizione	float,
 
@@ -75,8 +68,8 @@ costo_spedizione	float,
 		on delete no action
 		on update cascade,
         
-	foreign key (cliente)
-	references CLIENTE(id_cliente)
+	foreign key (utente)
+	references UTENTE(id_utente)
 		on delete no action
 		on update cascade
 );
@@ -124,7 +117,7 @@ CREATE TABLE PRODOTTO_IN_CATALOGO(
 id_prodotto		int			primary key,
 prezzo			float,
 iva			int,
-quantita 		int,
+quantitaÂ 		int,
 
 	foreign key (id_prodotto)
 	references PRODOTTO(id_prodotto)
@@ -136,7 +129,7 @@ CREATE TABLE PRODOTTO_IN_ORDINE(
 id_prodotto		int			primary key,
 prezzo			float,
 iva			int,
-quantita 		int,
+quantitaÂ 		int,
 
 	foreign key (id_prodotto)
 	references PRODOTTO(id_prodotto)
