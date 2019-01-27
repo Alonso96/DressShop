acfc98b838f1b441b32395b53fc7c7b74b754684
Rober-97 <Rober-97@users.noreply.github.com>
2019-01-27 19:03:49 +0100

    ulteriori modifiche accorpamento

--- DressShop/src/model/IndirizzoModelDM.java
+++ DressShop/src/model/IndirizzoModelDM.java
@@ -6,24 +6,23 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.LinkedList;
 
-public class UtenteModelDM implements UtenteModel<UtenteBean> {
-	
-	private static final String TABLE ="UTENTE";
+public class IndirizzoModelDM implements IndirizzoModel{
+
+	private static final String TABLE ="INDIRIZZO";
 
 	@Override
-	public UtenteBean doRetrieveByKey(int id_utente) throws SQLException {
+	public IndirizzoBean doRetrieveByKey(int id_indirizzo) throws SQLException {
 		Connection connection = null;
 		PreparedStatement statement=null;
-		UtenteBean bean = new UtenteBean();
+		IndirizzoBean bean = new IndirizzoBean();
 		
-		String queryString ="Select * FROM " + TABLE + " WHERE id_utente = ?";
+		String queryString ="Select * FROM " + TABLE + " WHERE id_indirizzo = ?";
 		
 		try{
 			connection = (Connection) DriverManagerConnectionPool.getConnection();
 			statement = (PreparedStatement) connection.prepareStatement(queryString);
-			statement.setInt(1, id_utente);
+			statement.setInt(1, id_indirizzo);
 			ResultSet result = statement.executeQuery();
 			while(result.next()){
 				bean = getBean(result);
@@ -36,10 +35,10 @@ public class UtenteModelDM implements UtenteModel<UtenteBean> {
 	}
 
 	@Override
-	public Collection<UtenteBean> doRetrieveAll() throws SQLException {
+	public Collection<IndirizzoBean> doRetrieveAll() throws SQLException {
 		Connection connection = null;
 		PreparedStatement statement=null;
-		Collection<UtenteBean> listaBean = new ArrayList<UtenteBean>();
+		Collection<IndirizzoBean> listaBean = new ArrayList<IndirizzoBean>();
 		
 		String queryString ="Select * FROM " + TABLE ;
 		try{
@@ -58,23 +57,24 @@ public class UtenteModelDM implements UtenteModel<UtenteBean> {
 	}
 
 	@Override
-	public void doSave(UtenteBean utente) throws SQLException {
+	public void doSave(IndirizzoBean indirizzo) throws SQLException {
 		Connection connection = null;
 		PreparedStatement statement=null;
 
-		String insertString=" INSERT INTO " + TABLE + " (nome, cognome, email, "
-				+ "password, tipo, data_nascita) VALUES(?, ?, ?, ?, ?, ?)";
+		String insertString=" INSERT INTO " + TABLE + " (nome, cognome, cap, "
+				+ "provincia, citta, via, cellulare) VALUES(?, ?, ?, ?, ?, ?, ?)";
 		
 		try{ 
 			connection = (Connection) DriverManagerConnectionPool.getConnection();
 			statement = (PreparedStatement) connection.prepareStatement(insertString);
 
-			statement.setString(1, utente.getNome());
-			statement.setString(2, utente.getCognome());
-			statement.setString(3, utente.getEmail());
-			statement.setString(4, utente.getPassword());
-			statement.setString(5, utente.getTipo());
-			statement.setDate(6, utente.getData_nascita());
+			statement.setString(1, indirizzo.getNome());
+			statement.setString(2, indirizzo.getCognome());
+			statement.setString(3, indirizzo.getCap());
+			statement.setString(4, indirizzo.getProvincia());
+			statement.setString(5, indirizzo.getCitta());
+			statement.setString(6, indirizzo.getVia());
+			statement.setString(7, indirizzo.getCellulare());
 			statement.executeUpdate();
 			
 			connection.commit();
@@ -85,24 +85,24 @@ public class UtenteModelDM implements UtenteModel<UtenteBean> {
 	}
 
 	@Override
-	public void doUpdate(UtenteBean utente) throws SQLException {
+	public void doUpdate(IndirizzoBean indirizzo) throws SQLException {
 		Connection connection = null;
 		PreparedStatement statement = null;
 
-		String insertSQL = "UPDATE " + TABLE + " SET nome = ?, cognome = ?, email = ?,"
-				+ " password = ?, tipo = ?, data_nascita = ? WHERE id_utente = ?;";
+		String insertSQL = "UPDATE " + TABLE + " SET nome = ?, cognome = ?, cap = ?,"
+				+ " provincia = ?, citta = ?, via = ?, cellulare = ? WHERE id_indirizzo = ?;";
 
 		try {
 			connection = DriverManagerConnectionPool.getConnection();
 			statement = connection.prepareStatement(insertSQL);
 			
-			statement.setString(1, utente.getNome());
-			statement.setString(2, utente.getCognome());
-			statement.setString(3, utente.getEmail());
-			statement.setString(4, utente.getPassword());
-			statement.setString(5, utente.getTipo());
-			statement.setDate(6, utente.getData_nascita());
-			statement.setInt(7, utente.getId_utente());
+			statement.setString(1, indirizzo.getNome());
+			statement.setString(2, indirizzo.getCognome());
+			statement.setString(3, indirizzo.getCap());
+			statement.setString(4, indirizzo.getProvincia());
+			statement.setString(5, indirizzo.getCitta());
+			statement.setString(6, indirizzo.getVia());
+			statement.setString(7, indirizzo.getCellulare());
 			statement.executeUpdate();
 
 			connection.commit();
@@ -113,17 +113,17 @@ public class UtenteModelDM implements UtenteModel<UtenteBean> {
 	}
 
 	@Override
-	public boolean doDelete(int id_utente) throws SQLException {
+	public boolean doDelete(int id_indirizzo) throws SQLException {
 		Connection connection = null;
 		PreparedStatement statement=null;
 		int result = 0;
 		
-		String deleteString ="DELETE FROM " + TABLE + " WHERE id_utente = ?";
+		String deleteString ="DELETE FROM " + TABLE + " WHERE id_indirizzo = ?";
 		
 		try {
 			connection = (Connection) DriverManagerConnectionPool.getConnection();
 			statement = (PreparedStatement) connection.prepareStatement(deleteString);
-			statement.setInt(1, id_utente);
+			statement.setInt(1, id_indirizzo);
 			
 			result = statement.executeUpdate();
 			
@@ -137,17 +137,17 @@ public class UtenteModelDM implements UtenteModel<UtenteBean> {
 	}
 
 	@Override
-	public UtenteBean doRetrieveByEmail(String email) throws SQLException {
+	public IndirizzoBean doRetrieveByUtente(int utente) throws SQLException {
 		Connection connection = null;
 		PreparedStatement statement=null;
-		UtenteBean bean = new UtenteBean();
+		IndirizzoBean bean = new IndirizzoBean();
 		
-		String queryString ="Select * FROM " + TABLE + " WHERE email = ?";
+		String queryString ="Select * FROM " + TABLE + " WHERE utente = ?";
 		
 		try{
 			connection = (Connection) DriverManagerConnectionPool.getConnection();
 			statement = (PreparedStatement) connection.prepareStatement(queryString);
-			statement.setString(1, email);
+			statement.setInt(1, utente);
 			ResultSet result = statement.executeQuery();
 			while(result.next()){
 				bean = getBean(result);
@@ -160,18 +160,19 @@ public class UtenteModelDM implements UtenteModel<UtenteBean> {
 	}
 	
 	
-	private static UtenteBean getBean(ResultSet rs) throws SQLException{
-		UtenteBean bean = new UtenteBean();
+	private static IndirizzoBean getBean(ResultSet rs) throws SQLException{
+		IndirizzoBean bean = new IndirizzoBean();
 		
-		bean.setId_utente(rs.getInt("id_utente"));
+		bean.setId_indirizzo(rs.getInt("id_indirizzo"));
 		bean.setNome(rs.getString("nome"));
 		bean.setCognome(rs.getString("cognome"));
-		bean.setEmail(rs.getString("email"));
-		bean.setPassword(rs.getString("password"));
-		bean.setTipo(rs.getString("tipo"));
-		bean.setData_nascita(rs.getDate("data_nascita"));
+		bean.setCap(rs.getString("cap"));
+		bean.setProvincia(rs.getString("provincia"));
+		bean.setCitta(rs.getString("citta"));
+		bean.setVia(rs.getString("via"));
+		bean.setCellulare(rs.getString("cellulare"));
+		bean.setUtente(rs.getInt("utente"));
 		
 		return bean;
 	}
-
 }
