67f2a667c946162486ee2c4fcd2287178178ee6a
Rober-97 <43641606+Rober-97@users.noreply.github.com>
2019-02-05 17:22:52 +0100

    Update inserimento tuple.sql

--- Bozze/DB/inserimento tuple.sql
+++ Bozze/DB/inserimento tuple.sql
@@ -2,7 +2,7 @@ use DressShop;
 
 insert into UTENTE(nome, cognome, email, password, tipo)
 values('mario', 'bianchi', 'uno@due', 'root', 1), ('marco', 'rossi', 'due@tre', 'root', 4), 
-('antonio', 'verdi', 'tre@quattro', 'root', 2), ('luca', 'gialli', 'quattro@cinque', 'root', 3);
+('antonio', 'verdi', 'tre@quattro', 'root', 2), ('luca', 'neri', 'quattro@cinque', 'root', 3);
 
 insert into CARTA_CREDITO(numero_carta, data_scadenza, cvv, nome_proprietario, cognome_proprietario, utente)
 values('4444444444444444', '2020/10/20', '333', 'mario', 'bianchi', 1);
