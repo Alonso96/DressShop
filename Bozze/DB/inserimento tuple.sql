a99936330974824a5d70602df5021da263151ab4
Rober-97 <43641606+Rober-97@users.noreply.github.com>
2019-02-06 16:34:42 +0100

    Update inserimento tuple.sql
    modifica email e password

--- Bozze/DB/inserimento tuple.sql
+++ Bozze/DB/inserimento tuple.sql
@@ -1,8 +1,8 @@
 use DressShop;
 
 insert into UTENTE(nome, cognome, email, password, tipo)
-values('mario', 'bianchi', 'uno@due', 'root', 1), ('marco', 'rossi', 'due@tre', 'root', 4), 
-('antonio', 'verdi', 'tre@quattro', 'root', 2), ('luca', 'neri', 'quattro@cinque', 'root', 3);
+values('mario', 'bianchi', 'uno@due.it', 'Password1', 1), ('marco', 'rossi', 'due@tre.it', 'Password1', 4), 
+('antonio', 'verdi', 'tre@quattro.it', 'Password1', 2), ('luca', 'neri', 'quattro@cinque.it', 'Password1', 3);
 
 insert into CARTA_CREDITO(numero_carta, data_scadenza, cvv, nome_proprietario, cognome_proprietario, utente)
 values('4444444444444444', '2020/10/20', '333', 'mario', 'bianchi', 1);
